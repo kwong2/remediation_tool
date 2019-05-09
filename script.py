a650668fc70868432df457dab79d7269e339b523
Ahmad Bilal <40148920+billyboyballin@users.noreply.github.com>
2019-05-09 09:16:56 -0400

    update_2

--- script.py
+++ script.py
@@ -5,23 +5,26 @@ Created on Wed May  8 18:57:37 2019
 @author: AhmadBilal
 """
 
+#Import packages
 import boto3
 import pandas as pd
 import json
 
+#Switch to appropriate role/account
 account = input("Insert account name: ")
 boto3.setup_default_session(profile_name=account)
 
-
+#Initiate ec2 session
+#Create filter for instances with cloudhesivemanaged tag = true 
+#Import using describe_instances
 ec2 = boto3.client('ec2')
-
 custom_filter = [{'Name':'tag:cloudhesivemanaged',
                   'Values':['true']}]
-
 response = ec2.describe_instances(Filters = custom_filter)
-response = response['Reservations']
 
+#Parse response object, extract InstanceID & launch date, append to list
 list = []
+response = response['Reservations']
 for i in response:
     for p in i['Instances']:
         month = str(p['LaunchTime'].month)
@@ -31,19 +34,19 @@ for i in response:
         list.append([p['InstanceId'],date])
 
 
+#Create dataframe
 df = pd.DataFrame(list, columns = ['instance_id', 'launch_time'])
 
-
+#Import datadog json data
 with open("datadog.json") as file:
     jsondata = json.load(file)
     
+#Capture InstanceID's that don't have datadog agent, add column to dataframe
 datadog_list = df['instance_id'].tolist()
 datadog_not_installed = []
-
 for host in jsondata['rows']:
     if str(host['display_name']) in datadog_list and ('agent' not in host['apps']):
         datadog_not_installed.append(host['display_name'])
-
 s = df['instance_id']
 df['datadog_installed'] = s.isin(datadog_not_installed)
 
